5ae9f089eada6a90bc52ea25f421d6b453eb5271
Matthew Oliver <protogonoi@gmail.com>
2014-09-02 19:39:00 +1000

    Add VS project files.

---
+++ SMC/version.h
@@ -0,0 +1,8 @@
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "XZ Utils 5.0.5"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.0.5"
+
+/* Version number of package */
+#define VERSION "5.0.5"
