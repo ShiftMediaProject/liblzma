cb407dd80cfdcbbe2d9d55c4ef7104141c981c3f
Matthew Oliver <protogonoi@gmail.com>
2014-09-16 17:04:10 +1000

    Update VS projects for 5.0.6 version.

--- SMC/version.h
+++ SMC/version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "XZ Utils 5.0.5"
+#define PACKAGE_STRING "XZ Utils 5.0.6"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.0.5"
+#define PACKAGE_VERSION "5.0.6"
 
 /* Version number of package */
-#define VERSION "5.0.5"
+#define VERSION "5.0.6"
